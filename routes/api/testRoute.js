f09942960a68bef5ac723ed8b885e292c4e18079
Oleg Lozovik <o.lozovik@gmail.com>
2022-01-14 16:54:15 +0200

    fixed testRoute.js

--- routes/api/testRoute.js
+++ routes/api/testRoute.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const { ctrlWrapper } = require("../../middlewares");
 
 const { testList } = require("../../controllers");
 const { ctrlWrapper } = require("../../middlewares");
