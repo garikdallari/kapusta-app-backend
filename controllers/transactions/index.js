78c3bc147f016120e0e5e05f21ce8480bfe61c18
ASTOUND\o.myronenko <o.myronenko@astoundcommerce.com>
2022-01-16 08:37:38 +0200

    updateBalance-endpoint: added updateBalance & createTransaction endpoints

--- controllers/transactions/index.js
+++ controllers/transactions/index.js
@@ -1,9 +1,11 @@
 const listTransactions = require('./listTransactions');
 const createTransaction = require('./createTransaction');
 const getAllByMonth = require('./getAllByMonth');
+const updateBalance = require('./updateBalance');
 
 module.exports = {
   listTransactions,
   createTransaction,
   getAllByMonth,
+  updateBalance,
 };
--- routes/api/transactionsRounter.js
+++ routes/api/transactionsRounter.js
@@ -5,11 +5,13 @@ const {
   listTransactions,
   createTransaction,
   getAllByMonth,
+  updateBalance,
 } = require('../../controllers');
 const { authenticate, ctrlWrapper } = require('../../middlewares');
 
 router.get('/', ctrlWrapper(listTransactions));
 router.post('/', authenticate, ctrlWrapper(createTransaction));
 router.get('/getAllByMonth/:date', authenticate, ctrlWrapper(getAllByMonth));
+router.patch('/updateBalance', authenticate, ctrlWrapper(updateBalance));
 
 module.exports = router;
