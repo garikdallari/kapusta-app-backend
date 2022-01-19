86b5458e4bf742505ec8008dab082a6bcb52e078
ASTOUND\o.myronenko <o.myronenko@astoundcommerce.com>
2022-01-19 14:38:26 +0200

    changed var name

--- controllers/transactions/getAllByMonth.js
+++ controllers/transactions/getAllByMonth.js
@@ -4,16 +4,16 @@ const getAllByMonth = async (req, res) => {
   const { date } = req.params;
   const [month, year] = date.split('-');
 
-  const data = await Transaction.find({ owner: req.user._id });
+  const result = await Transaction.find({ owner: req.user._id });
 
-  const result = data.filter(
+  const data = result.filter(
     ({ date }) => date.month === month && date.year === year,
   );
 
   res.json({
     status: 'success',
     code: 200,
-    result,
+    data,
   });
 };
 
